896bca14def411d274a83f4b92970a254cdf9b4a
Kevin Huang <huangk11@uci.edu>
2021-02-13 15:07:36 -0800

    integrated firebase into project

--- app/src/main/java/com/example/cs125project/ProfileFragment.java
+++ app/src/main/java/com/example/cs125project/ProfileFragment.java
@@ -3,22 +3,32 @@ package com.example.cs125project;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class ProfileFragment extends Fragment  {
     private Uri selectedImage = null;
     @Nullable
     @Override
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view =  inflater.inflate(R.layout.fragment_profile, container, false);
+        final View view =  inflater.inflate(R.layout.fragment_profile, container, false);
         ImageButton profileImage = (ImageButton) view.findViewById(R.id.profile_avatar);
         profileImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,11 +37,40 @@ public class ProfileFragment extends Fragment  {
             }
         });
         Button saveData = (Button) view.findViewById(R.id.save_button);
+
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference dbRef = database.getReference("user_name");
+
+        dbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                EditText name = (EditText) view.findViewById(R.id.profile_name);
+                name.setText(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+            }
+        });
+
         saveData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //save users changes to profile to firebase
                 //String userName
+
+                TextView name = (TextView) v.findViewById(R.id.profile_name);
+                String username = name.getText().toString();
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference dbRef = database.getReference("user_name");
+                dbRef.setValue(username);
+
+
             }
         });
         return view;
