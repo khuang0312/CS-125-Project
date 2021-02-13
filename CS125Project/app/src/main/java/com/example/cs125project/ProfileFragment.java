b09e718308345799f8449e795d777eac88e12fc1
krayskiy <56373342+krayskiy@users.noreply.github.com>
2021-02-13 13:28:58 -0800

    profile template

--- CS125Project/app/src/main/java/com/example/cs125project/ProfileFragment.java
+++ CS125Project/app/src/main/java/com/example/cs125project/ProfileFragment.java
@@ -1,19 +1,40 @@
 package com.example.cs125project;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class ProfileFragment extends Fragment {
-
+public class ProfileFragment extends Fragment  {
+    private Uri selectedImage = null;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view =  inflater.inflate(R.layout.fragment_profile, container, false);
+        ImageButton profileImage = (ImageButton) view.findViewById(R.id.profile_avatar);
+        profileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //upload image using firebase
+            }
+        });
+        Button saveData = (Button) view.findViewById(R.id.save_button);
+        saveData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //save users changes to profile to firebase
+                //String userName
+            }
+        });
+        return view;
     }
 }
+
